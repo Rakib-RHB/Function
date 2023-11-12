6d02ff2e22df14caf1ce172fd477de0b8bf36853
Rakib Hasan <140387583+Rakib-RHB@users.noreply.github.com>
2023-11-12 22:46:39 +0600

    Add files via upload

---
+++ Reusability.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void like()
+{
+    printf("Like ");
+}
+
+
+int main()
+{
+    int i;
+    printf("I ");
+    for(i = 0; i< 5; i++)
+    {
+        like();
+
+    }
+    printf("C ");
+}
