66b92f920c8ba594dbb8a3e3ba82b55e213632a6
Rakib Hasan <140387583+Rakib-RHB@users.noreply.github.com>
2023-11-12 22:47:00 +0600

    Add files via upload

---
+++ Reusability Using Parameter.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void like(int i)
+{
+    for(int n = 0; n <= i;n++)
+    {
+            printf("Like ");
+
+    }
+}
+
+
+int main()
+{
+    int i;
+    printf("I ");
+
+    like(3);
+
+    printf("C ");
+}
+
